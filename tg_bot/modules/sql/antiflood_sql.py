d082d34ad6be31f978f222486b8eb4c2897cf70c
੮૦қע૦ ૭Һ૦υՆ <75022139+TokyoGhoul2077@users.noreply.github.com>
2020-12-20 16:37:25 +0530

    Update antiflood_sql.py

--- tg_bot/modules/sql/antiflood_sql.py
+++ tg_bot/modules/sql/antiflood_sql.py
@@ -3,10 +3,9 @@ import threading
 from sqlalchemy import String, Column, Integer, UnicodeText
 
 from tg_bot.modules.sql import SESSION, BASE
-DEF_COUNT = 1
+DEF_COUNT = 0
 DEF_LIMIT = 0
 DEF_OBJ = (None, DEF_COUNT, DEF_LIMIT)
-
 
 class FloodControl(BASE):
     __tablename__ = "antiflood"
@@ -21,7 +20,6 @@ class FloodControl(BASE):
     def __repr__(self):
         return "<flood control for %s>" % self.chat_id
 
-
 class FloodSettings(BASE):
     __tablename__ = "antiflood_settings"
     chat_id = Column(String(14), primary_key=True)
@@ -34,8 +32,7 @@ class FloodSettings(BASE):
         self.value = value
 
     def __repr__(self):
-        return "<{} will executing {} for flood.>".format(
-            self.chat_id, self.flood_type)
+        return "<{} will executing {} for flood.>".format(self.chat_id, self.flood_type)
 
 
 FloodControl.__table__.create(checkfirst=True)
@@ -97,8 +94,7 @@ def set_flood_strength(chat_id, flood_type, value):
     with INSERTION_FLOOD_SETTINGS_LOCK:
         curr_setting = SESSION.query(FloodSettings).get(str(chat_id))
         if not curr_setting:
-            curr_setting = FloodSettings(
-                chat_id, flood_type=int(flood_type), value=value)
+            curr_setting = FloodSettings(chat_id, flood_type=int(flood_type), value=value)
 
         curr_setting.flood_type = int(flood_type)
         curr_setting.value = str(value)
