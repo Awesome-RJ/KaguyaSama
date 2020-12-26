0ee8ce52409b2d7aadf0256da93aebafdaed2cf4
Noob Naveen <75022139+TechGuyNaveen@users.noreply.github.com>
2020-12-26 14:38:14 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -24,5 +24,5 @@ if '%errorlevel%' NEQ '0' (
     pushd "%CD%"
     CD /D "%~dp0"
 :--------------------------------------
-net stop ShinobuSlayerBot
-net start ShinobuSlayerBot
+net stop SakuraBot
+net start SakuraBot
