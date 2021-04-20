1e8e1fcf654999d018b5be6c15b0b4493a302f81
Awesome Rajkumar <71130660+Rajkumar-27@users.noreply.github.com>
2021-04-20 12:33:44 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -2,9 +2,9 @@
 TITLE Kaguya Robot
 rem This next line removes any fban csv files if they exist in root when bot restarts. 
 del *.csv
-py -3.9.2 --version
+py -3.7 --version
 IF "%ERRORLEVEL%" == "0" (
-    py -3.9.2 -m tg_bot
+    py -3.7 -m tg_bot
 ) ELSE (
     py -m tg_bot
 )
